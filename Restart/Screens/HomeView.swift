0e6027a6a822994d2b0fc3214c8db3e17ba4fe89
Nic Deane <njdeane7@gmail.com>
2021-11-26 20:34:40 +1000

    Basic design of home screen done.

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -13,14 +13,38 @@ struct HomeView: View {
   
   var body: some View {
     VStack(spacing: 20) {
-      Text("Home")
-        .font(.largeTitle)
+      // MARK: - HEADER
+      Spacer()
+      
+      Image("character-2")
+        .resizable()
+        .scaledToFit()
+        .padding()
+      
+      // MARK: - CENTER
+      Text("The time that leads to mastery is dependent on our focus.")
+        .font(.title3)
+        .fontWeight(.light)
+        .foregroundColor(.secondary)
+        .multilineTextAlignment(.center)
+        .padding()
+      
+      // MARK: - FOOTER
+      Spacer()
       
       Button {
         isOnboardingViewActive = true
       } label: {
+        Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+          .imageScale(.large)
+        
         Text("Restart")
-      }
+          .font(.system(.title3, design: .rounded))
+          .fontWeight(.bold)
+      } //: BUTTON
+      .buttonStyle(.borderedProminent)
+      .buttonBorderShape(.capsule)
+      .controlSize(.large)
 
     } //: VSTACK
   }

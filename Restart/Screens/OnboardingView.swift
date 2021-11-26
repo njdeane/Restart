29bc07781c586e572c0d99dbef7262ca589d4584
Nic Deane <njdeane7@gmail.com>
2021-11-26 18:51:14 +1000

    Basic onboarding screen design done. Need to create animations to finish it.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,17 +12,101 @@ struct OnboardingView: View {
   @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
   
   var body: some View {
-    VStack(spacing: 20) {
-      Text("Onboarding")
-        .font(.largeTitle)
+    ZStack {
+      Color("ColorBlue")
+        .ignoresSafeArea(.all, edges: .all)
       
-      Button {
-        isOnboardingViewActive = false
-      } label: {
-        Text("Start")
-      }
-
-    } //: VSTACK
+      VStack(spacing: 20) {
+        // MARK: - Header
+        Spacer()
+        
+        VStack(spacing: 0) {
+          Text("Share.")
+            .font(.system(size: 60))
+            .fontWeight(.heavy)
+            .foregroundColor(.white)
+          
+          Text("""
+          It's not how much we give but
+          how much love we put into giving.
+          """)
+            .font(.title3)
+            .fontWeight(.light)
+            .foregroundColor(.white)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal, 10)
+        } //: HEADER
+        
+        // MARK: - Center
+        ZStack {
+          ZStack {
+            Circle()
+              .stroke(.white.opacity(0.2), lineWidth: 40)
+              .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+              .stroke(.white.opacity(0.2), lineWidth: 80)
+              .frame(width: 260, height: 260, alignment: .center)
+          } //: ZSTACK
+          Image("character-1")
+            .resizable()
+            .scaledToFit()
+        } //: CENTER
+        
+        Spacer()
+        
+        // MARK: - Footer
+        ZStack {
+          // PARTS OF THE CUSTOM BUTTON
+          
+          // 1. BACKGROUND (STATIC)
+          Capsule()
+            .fill(.white.opacity(0.2))
+          
+          Capsule()
+            .fill(.white.opacity(0.2))
+            .padding(8)
+          
+          // 2. CALL-TO-ACTION (STATIC)
+          Text("Get Started")
+            .font(.system(.title3, design: .rounded))
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .offset(x: 20)
+          
+          // 3. CAPSULE (DYNAMIC WIDTH)
+          HStack {
+            Capsule()
+              .fill(Color("ColorRed"))
+              .frame(width: 80)
+            
+            Spacer()
+          }
+          
+          // 4. CIRCLE (DRAGGABLE)
+          HStack {
+            ZStack {
+              Circle()
+                .fill(Color("ColorRed"))
+              Circle()
+                .fill(.black.opacity(0.15))
+                .padding(8)
+              Image(systemName: "chevron.right.2")
+                .font(.system(size: 24, weight: .bold))
+            }
+            .foregroundColor(.white)
+          .frame(width: 80, height: 80, alignment: .center)
+          .onTapGesture {
+            isOnboardingViewActive = false
+          }
+            
+            Spacer()
+          } //: HSTACK
+        } //: FOOTER
+        .frame(height: 80, alignment: .center)
+        .padding()
+        
+      } //: VSTACK
+    } //: ZSTACK
   }
 }
 

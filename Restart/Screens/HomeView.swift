77f7283504b1f735dd3109bc6a890029215a72a8
Nic Deane <njdeane7@gmail.com>
2021-12-07 19:04:14 +1000

    Add animations, sound and haptics to project.

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct HomeView: View {
   
   @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+  @State private var isAnimating: Bool = false
   
   var body: some View {
     VStack(spacing: 20) {
@@ -22,7 +23,15 @@ struct HomeView: View {
         Image("character-2")
           .resizable()
           .scaledToFit()
-        .padding()
+          .padding()
+          .offset(y: isAnimating ? 35 : -35)
+          .animation(
+            Animation
+              .easeInOut(duration: 4)
+              .repeatForever()
+            , value: isAnimating
+          )
+            
       }
       
       // MARK: - CENTER
@@ -37,7 +46,10 @@ struct HomeView: View {
       Spacer()
       
       Button {
-        isOnboardingViewActive = true
+        withAnimation {
+          playSound(sound: "success", type: "m4a")
+          isOnboardingViewActive = true
+        }
       } label: {
         Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
           .imageScale(.large)
@@ -49,8 +61,13 @@ struct HomeView: View {
       .buttonStyle(.borderedProminent)
       .buttonBorderShape(.capsule)
       .controlSize(.large)
-
+      
     } //: VSTACK
+    .onAppear {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+        isAnimating = true
+      }
+    }
   }
 }
 
